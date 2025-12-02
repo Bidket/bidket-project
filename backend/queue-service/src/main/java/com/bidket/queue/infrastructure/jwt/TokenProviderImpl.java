6691472a248ebce6690a2bb6cd6bd60b20c95b46
rudcjs <rudcjs335@gmail.com>
2025-12-02 11:30:49 +0900

    feat: 토큰 생성

--- backend/queue-service/src/main/java/com/bidket/queue/infrastructure/jwt/TokenProviderImpl.java
+++ backend/queue-service/src/main/java/com/bidket/queue/infrastructure/jwt/TokenProviderImpl.java
@@ -2,22 +2,29 @@ package com.bidket.queue.infrastructure.jwt;
 
 import com.bidket.queue.domain.jwt.TokenProvider;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
 import java.util.UUID;
 
 public class TokenProviderImpl implements TokenProvider {
-    private Key key;
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private long expiration;
 
     @Override
     public String generateToken(UUID userId, UUID auctionId) {
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secret));
         return Jwts.builder()
                 .claim("userId", userId)
                 .claim("acutionId", auctionId)
                 .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plusSeconds(expiration)))
                 .signWith(key)
                 .compact();
     }
