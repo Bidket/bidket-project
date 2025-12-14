2c2494ef53b78fe33ea569230ca183f7f1dfa834
yeana07 <lee.ye.naaa@gmail.com>
2025-12-14 20:07:34 +0900

    feat: 어드민 상품 목록 조회 및 상품 단건 조회 구현
      - 어드민 상품 목록 조회에 Specification 적용
      - 상품 상태, 상품 타입, 브랜드 기준 필터링
    
    (#84)

--- backend/product-service/src/main/java/com/bidket/product/application/service/AdminQueryService.java
+++ backend/product-service/src/main/java/com/bidket/product/application/service/AdminQueryService.java
@@ -1,12 +1,21 @@
 package com.bidket.product.application.service;
 
 import com.bidket.common.presentation.response.PageResponse;
+import com.bidket.product.application.mapper.AdminProductMapper;
 import com.bidket.product.application.mapper.AdminSizeMapper;
+import com.bidket.product.domain.exception.ProductErrorCode;
+import com.bidket.product.domain.exception.ProductException;
+import com.bidket.product.domain.model.ProductStatus;
+import com.bidket.product.infrastructure.persistence.entity.Product;
 import com.bidket.product.infrastructure.persistence.entity.Size;
 import com.bidket.product.infrastructure.persistence.entity.SizeType;
+import com.bidket.product.infrastructure.persistence.repository.ProductAdminSpecification;
+import com.bidket.product.infrastructure.persistence.repository.ProductRepository;
 import com.bidket.product.infrastructure.persistence.repository.SizeRepository;
 import com.bidket.product.infrastructure.persistence.repository.SizeTypeRepository;
 import com.bidket.product.presentation.dto.request.PageRequestDto;
+import com.bidket.product.presentation.dto.response.product.ProductGetAdminResponse;
+import com.bidket.product.presentation.dto.response.product.ProductGetAdminSimpleResponse;
 import com.bidket.product.presentation.dto.response.size.SizeGetAdminResponse;
 import com.bidket.product.presentation.dto.response.size.SizeTypeGetAdminResponse;
 import java.util.List;
@@ -16,6 +25,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +36,10 @@ public class AdminQueryService {
 
     private final SizeTypeRepository sizeTypeRepository;
     private final SizeRepository sizeRepository;
+    private final ProductRepository productRepository;
+
     private final AdminSizeMapper adminSizeMapper;
+    private final AdminProductMapper adminProductMapper;
 
     /** 사이즈 타입 목록 조회 */
     public PageResponse<SizeTypeGetAdminResponse> getSizeTypes(
@@ -75,5 +88,43 @@ public class AdminQueryService {
                 page.getSize(),
                 page.getTotalElements()
         );
+    }
+
+    /** 상품 목록 조회 */
+    public PageResponse<ProductGetAdminResponse> getProducts(
+            ProductStatus status,
+            UUID brandId,
+            UUID productTypeId,
+            PageRequestDto pageRequest
+    ) {
+        Pageable pageable = pageRequest.toPageable(Sort.by(Direction.DESC, "createdAt")
+        );
+
+        Specification<Product> spec = ProductAdminSpecification.hasStatus(status)
+                .and(ProductAdminSpecification.hasBrand(brandId))
+                .and(ProductAdminSpecification.hasProductType(productTypeId));
+
+        Page<Product> page = productRepository.findAll(spec, pageable);
+
+        List<ProductGetAdminResponse> resList = page.getContent().stream()
+                .map(adminProductMapper::toListDto)
+                .toList();
+
+        return PageResponse.of(
+                resList,
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements()
+        );
+    }
+
+    /** 상품 단건 단순 조회 */
+    public ProductGetAdminSimpleResponse getProduct(
+            UUID productId
+    ) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductException(ProductErrorCode.PRODUCT_NOT_FOUND));
+
+        return adminProductMapper.toSimpleDto(product);
     }
 }
--- backend/product-service/src/main/java/com/bidket/product/presentation/api/AdminQueryController.java
+++ backend/product-service/src/main/java/com/bidket/product/presentation/api/AdminQueryController.java
@@ -3,18 +3,24 @@ package com.bidket.product.presentation.api;
 import com.bidket.common.presentation.response.ApiResponse;
 import com.bidket.common.presentation.response.PageResponse;
 import com.bidket.product.application.service.AdminQueryService;
+import com.bidket.product.domain.model.ProductStatus;
 import com.bidket.product.presentation.dto.request.PageRequestDto;
+import com.bidket.product.presentation.dto.response.product.ProductGetAdminResponse;
+import com.bidket.product.presentation.dto.response.product.ProductGetAdminSimpleResponse;
 import com.bidket.product.presentation.dto.response.size.SizeGetAdminResponse;
 import com.bidket.product.presentation.dto.response.size.SizeTypeGetAdminResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/admin")
 @RequiredArgsConstructor
@@ -51,4 +57,41 @@ public class AdminQueryController {
         return ApiResponse.success(adminQueryService.getSizes(sizeTypeId, pageRequest));
     }
 
+    @Operation(
+            summary = "어드민 상품 목록 조회",
+            description = "어드민이 모든 상태의 상품 목록을 조회합니다. "
+                    + "상태값, 특정 상품 타입ID, 특정 브랜드 ID로 필터링할 수 있습니다."
+    )
+    @GetMapping("/products")
+    public ApiResponse<PageResponse<ProductGetAdminResponse>> getProducts(
+            //@RequestHeader("X-User-Role") String role,
+            @RequestParam(defaultValue = "ALL") ProductStatus status,
+            @RequestParam(required = false) UUID brandId,
+            @RequestParam(required = false) UUID productTypeId,
+            PageRequestDto pageRequest
+    ) {
+        //adminRoleValidator.validator(role);
+        log.info("/v1/admin/products Received status: {}", status);
+        return ApiResponse.success(
+                adminQueryService.getProducts(
+                        status,
+                        brandId,
+                        productTypeId,
+                        pageRequest
+                )
+        );
+    }
+
+    @Operation(
+            summary = "어드민 상품 단건 조회",
+            description = "어드민이 상품 단건을 조회합니다. "
+    )
+    @GetMapping("/products/{productId}")
+    public ApiResponse<ProductGetAdminSimpleResponse> getProduct(
+            //@RequestHeader("X-User-Role") String role,
+            @PathVariable UUID productId
+    ) {
+        //adminRoleValidator.validator(role);
+        return ApiResponse.success(adminQueryService.getProduct(productId));
+    }
 }
