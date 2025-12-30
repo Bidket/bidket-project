b19e487d426c0777396c2843fa051484e62bfb82
yeana07 <lee.ye.naaa@gmail.com>
2025-12-30 09:19:12 +0900

    fix: cicd 이미지 태그 오류로 인한 배포 실패 문제 해결
      - 모든 서비스에 동일한 이미지 태그 적용하던 구조 삭제
      - 빌드된 서비스만 이미지 태그 갱신하도록 수정
      - run-app.sh에 이미지 태그 변경 로직 제거
      - deploy에서 이미지 태그 업데이트 로직 추가(sed)
    
    (#172)

--- run-app.sh
+++ run-app.sh
@@ -33,22 +33,6 @@ trap rollback ERR
 
 
 # ============================
-# function
-# ============================
-set_env() {
-  local KEY=$1
-  local VALUE=$2
-  local FILE=$3
-
-  if grep -q "^${KEY}=" "$FILE"; then
-    sed -i "s|^${KEY}=.*|${KEY}=${VALUE}|" "$FILE"
-  else
-    echo "${KEY}=${VALUE}" >> "$FILE"
-  fi
-}
-
-
-# ============================
 # Bidket App Deployment Script
 # ============================
 
@@ -61,24 +45,6 @@ cd "$APP_DIR"
 
 # Backup env
 cp "$ENV_FILE" "$BACKUP_FILE"
-
-# required env validation(태그 없는 배포 막기)
-: "${GATEWAY_IMAGE_TAG:?GATEWAY_IMAGE_TAG is required}"
-: "${USER_IMAGE_TAG:?USER_IMAGE_TAG is required}"
-: "${PRODUCT_IMAGE_TAG:?PRODUCT_IMAGE_TAG is required}"
-: "${ORDER_IMAGE_TAG:?ORDER_IMAGE_TAG is required}"
-: "${AUCTION_IMAGE_TAG:?AUCTION_IMAGE_TAG is required}"
-: "${NOTIFICATION_IMAGE_TAG:?NOTIFICATION_IMAGE_TAG is required}"
-: "${QUEUE_IMAGE_TAG:?QUEUE_IMAGE_TAG is required}"
-
-# Image Tag setting
-set_env GATEWAY_IMAGE_TAG "$GATEWAY_IMAGE_TAG" "$ENV_FILE"
-set_env USER_IMAGE_TAG "$USER_IMAGE_TAG" "$ENV_FILE"
-set_env PRODUCT_IMAGE_TAG "$PRODUCT_IMAGE_TAG" "$ENV_FILE"
-set_env ORDER_IMAGE_TAG "$ORDER_IMAGE_TAG" "$ENV_FILE"
-set_env AUCTION_IMAGE_TAG "$AUCTION_IMAGE_TAG" "$ENV_FILE"
-set_env NOTIFICATION_IMAGE_TAG "$NOTIFICATION_IMAGE_TAG" "$ENV_FILE"
-set_env QUEUE_IMAGE_TAG "$QUEUE_IMAGE_TAG" "$ENV_FILE"
 
 echo "📌 Step 2. 최신 이미지 Pull (app 전용)"
 docker compose \
